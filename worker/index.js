4e71e051918ce964223d37575cd9d62f7f282f21
Greenchik <green@ukr.net>
2019-11-15 18:04:30 +0200

    worker update

--- worker/index.js
+++ worker/index.js
@@ -15,7 +15,7 @@ function fib(index) {
 
 sub.on('message', (channel, message) => {
 //  redisClient.hset('values', message, fib(parseInt(message)));
-  redisClient.hset('values', message, 'LaLaLa');
+  redisClient.hset('values', message, fib(parseInt(message)));
   console.log('>>>' + message + '<<<')
 });
 sub.subscribe('insert');
